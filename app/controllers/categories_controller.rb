5350637ce5a95122390a66ab0901d3cbf0c773a1
Andrew Glass <andrewglass1@gmail.com>
2014-12-04 15:07:03 -0500

    more features on category#show

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -5,6 +5,7 @@ class CategoriesController < ApplicationController
     redirect_to root_path unless VALID_CATEGORIES.include?(params["id"])
     title_and_description
     @listings = Listing.send(params['id'].underscore)
+    meta_tags
   end
 
 private
@@ -14,9 +15,20 @@ private
     when 'brick-and-mortar'
       @category_title = "Brick and Mortar"
       @category_description = "Find the best ways to build a website for your brick and mortar business.  Great for restaurant, doctor's pratices, etc"
-    when "fff"
+    when "portfolios"
       @category_title = "Brick and Mortar"
-      @category_description = "Find the best ways to build a website for your brick and mortar business.  Great for restaurant, doctor's pratices, etc"
+      @category_description = "Find the best ways to build a website for your portfolio.  Great for artists, designers, musicians, etc."
+    when "mobile"
+      @category_title = "Mobile apps and websites"
+      @category_description = "Find the best ways to build a mobile app or website.  Great for iOS iPhone apps, Android apps and responsive websites."
+    when "e-commerce"
+      @category_title = "eCommerce"
+      @category_description = "Find the best ways to build a website for your eCommerce business.  Great for selling products via an online storefront."
     end
+  end
+
+  def meta_tags
+    @meta = {:title => "Build without code: Build #{params['id'].titleize} websites",
+             :description => "Find the best ways to build #{params['id'].titleize} websites without using code"}
   end
 end
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,13 @@
 class WelcomeController < ApplicationController
   def index
+    meta_tags
+  end
+
+private
+
+  def meta_tags
+    @meta = {:title => "Build Without Code",
+             :description => "Find the best ways to build websites without using code"
+    }
   end
 end
